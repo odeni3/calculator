c1476ee2c51ece85476c1f126557f08157d7b3c3
odeni3 <danielgpmeneses@gmail.com>
2024-01-09 18:59:14 -0300

    Primeiro commit

---
+++ src/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    background-color: #CACACA;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+export const Content = styled.div`
+    background-color: #FFFF;
+    width: 40rem;
+    min-height: 50rem;
+`
